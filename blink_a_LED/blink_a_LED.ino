07150843b5d7c023e81c2ffdcbf0e13435f7a352
bright123-max <brightathuman65@gmail.com>
2026-01-02 22:06:04 +0300

    first blinking a LED min project

---
+++ blink_a_LED/blink_a_LED.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  // putp your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(100);
+
+  // put your main code here, to run repeatedly:
+
+}
